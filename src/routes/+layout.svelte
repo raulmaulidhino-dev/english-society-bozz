d05e4715422de49fd2b2e52ba5be241657e2a7dc
Raul Maulidhino <raulahmadm314@gmail.com>
2024-10-07 17:55:27 +0700

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Header from './Header.svelte';
+	import '../app.css';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+	</footer>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>ES-Bozz - Home</title>
+	<meta name="description" content="English Society-Bozz Homepage" />
+</svelte:head>
+
+<section>
+    <h1>Welcome to ES-Bozz!</h1>
+</section>
