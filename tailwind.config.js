50b4da9738d9eba62a12e71e4aaeaec7cc81e6fc
Raul Maulidhino <raulahmadm314@gmail.com>
2024-10-08 23:13:03 +0700

    feat: add website font (Montserrat)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,16 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
-    colors: {
-      primary: '#0F286C',
-      secondary: '#EFC007',
-      background: '#FAFAFA',
-      text: '#020202',
+    extend: {
+      colors: {
+        primary: '#0F286C',
+        secondary: '#EFC007',
+        background: '#FAFAFA',
+        text: '#020202',
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'serif'],
+      },
     },
   },
   plugins: [],
