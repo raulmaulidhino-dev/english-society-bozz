831a0c6bb450345bce447cef585a65ba13d63cad
Raul Maulidhino <raulahmadm314@gmail.com>
2024-10-08 23:15:45 +0700

    feat: add Header section

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,16 @@
 <script>
+    import logo from '$lib/images/logos/esbozz-logo.webp';
+    import logo_fallback from '$lib/images/logos/esbozz-logo.png';
+    import Navigation from './Navigation.svelte';
 </script>
 
-<header>
+<header class="py-8 px-6 border-b-2 flex justify-between items-center">
+    <section class="logo max-w-48 flex justify-center items-center">
+        <picture>
+            <source type="image/webp" srcset={logo} />
+            <img src={logo_fallback} alt="English Society-Bozz logo" class="h-16" />
+        </picture>
+        <p class="font-montserrat font-bold text-sm">ENGLISH<br /> SOCIETY-BOZZ</p>
+    </section>
+    <Navigation />
 </header>
