9b5d9f631c8c802740e2a35e441b8b3db4d82526
Raul Maulidhino <raulahmadm314@gmail.com>
2024-10-11 23:49:50 +0700

    feat: add Hero section to engage visitors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script>
+	import Hero from "./Hero.svelte";
 </script>
 
 <svelte:head>
 	<title>ES-Bozz - Home</title>
 	<meta name="description" content="English Society-Bozz Homepage" />
 </svelte:head>
+
+<Hero />
 
 <section>
 </section>
---
+++ src/routes/Hero.svelte
@@ -0,0 +1,17 @@
+<script></script>
+
+<section class="px-4 py-6">
+    <section>
+        <h1 class="text-3xl font-bold mb-[0.5em]">Speak Up, Express Yourself: Join our Community!</h1>
+        <p class="text-[12px] text-gray-600 font-medium mb-[1em]">
+            Don't be shy to speak English - 
+            Try speaking up here, in one of the most active extracurriculars in SMA Negeri 1 Bobotsari,
+            <span class="text-primary font-semibold">English Society-Bozz!</span>
+        </p>
+        <section class="flex gap-4 mb-[1em]">
+            <button class="text-white text-sm px-4 py-2 font-bold bg-primary hover:text-primary hover:bg-secondary rounded-3xl inline-block">JOIN NOW</button>
+            <button class="text-black text-sm px-4 py-2 font-bold bg-secondary hover:text-secondary hover:bg-primary rounded-3xl inline-block">EXPLORE</button>
+        </section>
+        <p class="text-[10px]">*Before joining, make sure that you are one of the active students in SMA Negeri 1 Bobotsari</p>
+    </section>
+</section>
