b6deb410abcf60268ea80a06fe649e16347493df
Raul Maulidhino <raulahmadm314@gmail.com>
2024-10-23 04:48:18 +0700

    fix: improve layout's responsiveness

--- src/routes/Numbers.svelte
+++ src/routes/Numbers.svelte
@@ -19,14 +19,14 @@
 <section class="px-6 py-[6rem] flex flex-wrap justify-evenly gap-8">
     <section class="flex flex-wrap max-w-lg gap-4">
         {#each numbers as number, i}
-            <div class={`${(i+1) % 4 == 2 || (i+1) % 4 == 3 ? "bg-primary text-secondary" : "bg-secondary text-primary" } rounded-2xl p-4 aspect-square max-w-44 flex flex-col justify-center items-center gap-2`}>
-                <div class="text-5xl font-extrabold mx-auto">{number.count}+</div>
-                <div class="font-semibold mx-auto">{number.title}</div>
+            <div class={`${(i+1) % 4 == 2 || (i+1) % 4 == 3 ? "bg-primary text-secondary" : "bg-secondary text-primary" } rounded-2xl p-4 aspect-square basis-[calc(50%-8px)] flex flex-col justify-center items-center gap-2`}>
+                <div class="text-4xl md:text-5xl font-extrabold mx-auto">{number.count}+</div>
+                <div class="text-sm md:text-base font-semibold mx-auto">{number.title}</div>
             </div>
         {/each}
     </section>
     <section class="max-w-xl">
-        <div class="flex justify-between items-center gap-4">
+        <div class="flex flex-wrap justify-center items-center content-center gap-4">
             {#each doodles as doodle, i}
                 <img class={`border-4 ${i % 2 == 0 ? "border-primary" : "border-secondary"} object-cover w-32 rounded-full`} src={doodle.src} alt={doodle.alt} />
             {/each}
