05b50857ad78bcdf6e93d1cc1ce01dad60eecfcd
Raul Maulidhino <raulahmadm314@gmail.com>
2024-10-24 11:59:45 +0700

    fix: make Fun Activities layout responsive

--- src/routes/FunActivities.svelte
+++ src/routes/FunActivities.svelte
@@ -9,11 +9,14 @@
     ];
 </script>
 
-<section class="px-6 py-[6rem]">
-    <h2 class="text-2xl sm:text-3xl md:text-4xl text-primary font-bold my-[1em]">Look Up Our Fun Activities Here!</h2>
-    <section class="flex gap-4 overflow-auto">
+<section class="bg-secondary px-6 py-[4rem]">
+    <h2 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-primary text-center font-bold mx-auto mb-[4rem]">Look Up Our Fun Activities Here!</h2>
+    <section class="flex flex-wrap justify-center gap-4 md:flex-nowrap md:justify-start md:overflow-scroll">
         {#each activities as activity}
-            <section class={`text-white text-lg font-bold ${activity.bg} bg-cover min-w-72 aspect-[3/4] rounded-xl p-6 flex flex-col justify-end`}>{activity.title}</section>
+            <section class={`text-white text-lg font-bold ${activity.bg} bg-cover md:min-w-72 max-w-80 w-full aspect-[3/4] rounded-xl p-6 flex flex-col justify-end relative`}>
+                <div class="rounded-xl absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,0.8)] z-10"></div>
+                <span class="z-20">{activity.title}</span>
+            </section>
         {/each}
     </section>
 </section>
