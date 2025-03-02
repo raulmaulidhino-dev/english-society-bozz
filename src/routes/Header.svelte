6475461c91a894fbd049b7e5ba8a362b34f7aca0
Raul Maulidhino <raulahmadm314@gmail.com>
2025-03-02 12:07:38 +0700

    feat: add login button

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,6 +2,12 @@
     import logo from '$lib/images/logos/es-bozz-logo-transparent.webp';
     import logo_fallback from '$lib/images/logos/es-bozz-logo-transparent.png';
     import Navigation from './Navigation.svelte';
+    import { goto } from '$app/navigation';
+
+    async function login() {
+        await goto("/login");
+    }
+
 </script>
 
 <header class="py-8 px-6 border-b-2 flex justify-between items-center">
@@ -13,4 +19,6 @@
         <p class="font-montserrat font-bold text-sm">ENGLISH<br /> SOCIETY-BOZZ</p>
     </section>
     <Navigation />
+    <!-- I'll resolve this later on -->
+    <button class="text-white text-sm px-4 py-2 font-bold bg-primary hover:bg-primary_hover rounded-full" on:click={login}>LOGIN</button> 
 </header>
