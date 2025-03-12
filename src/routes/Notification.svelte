9b23d94b5bfb266f6241ce408a9b37d2b475341b
Raul Maulidhino <raulahmadm314@gmail.com>
2025-03-12 18:53:40 +0700

    feat: add dialog message box

---
+++ src/routes/Notification.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let message = "";
+    export let type = "success";
+    export let duration = 5000;
+
+    let visible = true;
+
+    const hideNotification = () => {
+        visible = false;
+    };
+
+    setTimeout(hideNotification, duration);
+</script>
+
+{#if visible}
+    <div class="fixed top-5 r-5 flex items-center justify-between p-4 rounded-lg shadow-lg text-white w-full max-w-80 transition-opacity duration-500 ease-in-out cursor-pointer { type === "error" ? 'bg-red-500' : 'bg-green-500' }">
+        <p>{ message }</p>
+        <button on:click={hideNotification} class="font-extrabold">x</button>
+    </div>
+{/if}
