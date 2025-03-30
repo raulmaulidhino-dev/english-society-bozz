8d1c15fbbe751f9a134eec10e585573508c51527
Raul Maulidhino <raulahmadm314@gmail.com>
2025-03-30 16:17:29 +0700

    feat: add Navigation Mobile menu on the main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
 	import Header from './Header.svelte';
+	import NavigationMobile from './NavigationMobile.svelte';
 	import Footer from './Footer.svelte';
 	import '../app.css';
 </script>
 
 <div class="app">
 	<Header />
+	<NavigationMobile />
 
 	<main>
 		<slot />
