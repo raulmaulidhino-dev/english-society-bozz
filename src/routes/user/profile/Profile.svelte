45b3d93be9642813fc09e977ebb291ac369ce570
Raul Maulidhino <raulahmadm314@gmail.com>
2025-03-31 19:48:04 +0700

    style: restyle profile page

--- src/routes/user/profile/Profile.svelte
+++ src/routes/user/profile/Profile.svelte
@@ -74,20 +74,34 @@
 </script>
 
 {#if !isLoading}
-    <section>
-        <section class="py-10 px-6 flex flex-col justify-center items-center gap-1">
-            <!-- svelte-ignore a11y-img-redundant-alt -->
-            <img src={defaultPFP} 
-                alt="User Photo Profile"
-                class="h-24 rounded-full my-6"
-            />
-            <h2 class="text-3xl text-primary font-bold">{userProfile?.full_name || "Full Name"}</h2>
-            <p class="text-sm text-slate-500">{userProfile?.users.username || "username"}</p>
-            <p class="text-sm text-center my-2 mx-4">{userProfile?.bio || "No bio yet."}</p>
-            <a href="/user/profile/edit" class="text-sm text-primary font-semibold border-2 border-primary py-2 px-8 rounded-full my-4
-                                        hover:bg-secondary">EDIT PROFILE</a>
-            <button class="text-sm text-white bg-red-500 font-semibold border-2 border-primary py-2 px-8 rounded-full my-4"
-                                        on:click={logout}>LOGOUT</button>
+    <section class="bg-slate-200 p-4 md:p-8">
+        <section class="bg-white max-w-xl rounded-2xl mx-auto shadow-lg flex flex-col justify-center">
+            <section class="bg-secondary w-full h-40 rounded-t-[inherit]">
+            </section>
+            <section class="rounded-b-[inherit] relative">
+                <section class="pb-10 px-6 profile-data">
+                    <!-- svelte-ignore a11y-img-redundant-alt -->
+                    <img src={defaultPFP} 
+                        alt="User Photo Profile"
+                        class="bg-white h-24 border-4 border-white rounded-full absolute top-[-48px] left-8"
+                    />
+                    <h2 class="text-2xl text-primary font-bold mt-14">{userProfile?.full_name || "Full Name"}</h2>
+                    <p class="text-sm text-slate-500">{userProfile?.users.username || "username"}</p>
+                    <p class="text-sm">{userProfile?.bio || "No bio yet."}</p>
+                </section>
+                <section class="border-t-2 border-slate-200 py-4 px-4 flex flex-col gap-2">
+                    <a href="/user/profile/edit" class="text-sm text-primary text-center font-semibold w-full sm:w-fit border-2 border-primary py-2 px-8 rounded-full
+                                            hover:bg-secondary sm:absolute sm:top-4 sm:right-4 inline-block">EDIT PROFILE</a>
+                    <button class="text-sm text-white bg-red-600 font-semibold w-full sm:w-fit border-2 border-primary py-2 px-8 rounded-full"
+                                            on:click={logout}>LOG OUT</button>
+                </section>
+            </section>
         </section>
     </section>
 {/if}
+
+<style>
+    .profile-data > *:not(h2) {
+        @apply mb-[1em];
+    }
+</style>
