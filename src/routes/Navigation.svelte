dd2aba3ba22746ff5a4c3f553036e046caee4a75
Raul Maulidhino <raulahmadm314@gmail.com>
2025-04-01 21:56:07 +0700

    feat: add hover effect on desktop Navigation items

--- src/routes/Navigation.svelte
+++ src/routes/Navigation.svelte
@@ -29,8 +29,38 @@
     <nav>
         <ul class="flex p-4 justify-around items-center gap-6">
             {#each navItems as navItem}
-                <li title={navItem.text} class="font-semibold"><a href={navItem.href} >{navItem.text}</a></li>
+                <li title={navItem.text} class="font-semibold">
+                    <a href={navItem.href} class="nav-item hover:text-primary">{navItem.text}</a>
+                </li>
             {/each}
         </ul>
     </nav>
 {/if}
+
+<style>
+    
+    .nav-item {
+        display: inline-block;
+        position: relative;
+    }
+
+    .nav-item::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 2px;
+        background-color: #EFC007;
+        visibility: hidden;
+        transform: scaleX(0);
+        transform-origin: bottom center;
+        transition: transform 0.3s ease-in-out;
+    }
+
+    .nav-item:hover::after {
+        visibility: visible;
+        transform: scaleX(1);
+    }
+
+</style>
