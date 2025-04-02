f5e7bdfbafa25010876b61ec6d91e59119d376ae
Raul Maulidhino <raulahmadm314@gmail.com>
2025-04-02 09:37:25 +0700

    style: implement the loading spinner feature

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,19 @@
 <script>
+	import {beforeUpdate, afterUpdate, onMount} from 'svelte';
+
 	import Hero from './Hero.svelte';
 	import Insights from './Insights.svelte';
 	import Numbers from './Numbers.svelte';
 	import FunActivities from './FunActivities.svelte';
 	import CapturedMoments from './CapturedMoments.svelte';
+
+	import LoadingSpinner from './loading/LoadingSpinner.svelte';
+
+	let isLoading = true;
+
+	onMount(() => {
+        isLoading = false;
+    });
 </script>
 
 <svelte:head>
@@ -11,8 +21,12 @@
 	<meta name="description" content="English Society-Bozz Homepage" />
 </svelte:head>
 
-<Hero />
-<Insights />
-<Numbers />
-<FunActivities />
-<CapturedMoments />
+{#if isLoading}
+	<LoadingSpinner />
+{:else}
+	<Hero />
+	<Insights />
+	<Numbers />
+	<FunActivities />
+	<CapturedMoments />
+{/if}
