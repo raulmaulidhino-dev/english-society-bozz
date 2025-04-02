61076e407a4bb1aee9b27affb09041fc92e6b02e
Raul Maulidhino <raulahmadm314@gmail.com>
2025-04-02 17:31:19 +0700

    fix: resolve dynamic height

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,10 +2,10 @@
     import { page } from '$app/stores';
 </script>
 
-<section class="bg-slate-200 p-4 md:p-8">
+<section class="bp:h-[max(calc(100vh-114px),500px)] max-h-[1080px] bg-slate-200 p-4 md:p-8">
     <section class="bg-white pt-16 pb-8 px-4 max-w-xl rounded-2xl mx-auto shadow-lg">
         {#if $page.status === 404}
-            <div class="mx-auto px-4 max-w-screen-sm bp:h-[max(calc(100vh-114px),500px)] max-h-[1080px] text-center flex flex-col justify-center items-center">
+            <div class="mx-auto px-4 max-w-screen-sm text-center flex flex-col justify-center items-center">
                 <h1 class="text-8xl tracking-tight font-extrabold text-primary lg:text-9xl text-primary-600 animate-bounce">4<span class="text-secondary">0</span>4</h1>
                 <div class="bg-slate-200 h-[2px] w-full rounded-full mb-4"></div>
                 <p class="mb-4 text-2xl tracking-tight font-bold text-gray-900 md:text-3xl">Something's missing.</p>
@@ -13,7 +13,7 @@
                 <a href="/" class="text-sm text-white text-center bg-primary font-bold w-full sm:w-fit border-2 border-primary py-2 px-8 rounded-full hover:text-primary hover:bg-secondary inline-block">GO TO HOMEPAGE</a>
             </div>
         {:else if $page.status === 500}
-            <div class="mx-auto px-4 max-w-screen-sm bp:h-[max(calc(100vh-114px),500px)] max-h-[1080px] text-center flex flex-col justify-center items-center">
+            <div class="mx-auto px-4 max-w-screen-sm text-center flex flex-col justify-center items-center">
                 <h1 class="text-8xl tracking-tight font-extrabold text-primary lg:text-9xl text-primary-600 animate-bounce">5<span class="text-secondary">0</span>0</h1>
                 <div class="bg-slate-200 h-[2px] w-full rounded-full mb-4"></div>
                 <p class="mb-4 text-2xl tracking-tight font-bold text-gray-900 md:text-3xl">We're experiencing a server issue.</p>
