49d8fb2936adc8ae876736751ebc483ae5a1ece6
Raul Maulidhino <raulahmadm314@gmail.com>
2025-04-06 17:03:49 +0700

    style: change title and meta content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,18 @@
 <script>
-	import {beforeUpdate, afterUpdate, onMount} from 'svelte';
-
 	import Hero from './Hero.svelte';
 	import Insights from './Insights.svelte';
 	import Numbers from './Numbers.svelte';
 	import FunActivities from './FunActivities.svelte';
 	import CapturedMoments from './CapturedMoments.svelte';
 
-	import LoadingSpinner from './loading/LoadingSpinner.svelte';
-
-	let isLoading = true;
-
-	onMount(() => {
-        isLoading = false;
-    });
 </script>
 
 <svelte:head>
-	<title>ES-Bozz - Home</title>
-	<meta name="description" content="English Society-Bozz Homepage" />
+	<title>ES-Bozz</title>
+	<meta name="description" content="English Society-Bozz Website" />
 </svelte:head>
-
-{#if isLoading}
-	<LoadingSpinner />
-{:else}
-	<Hero />
-	<Insights />
-	<Numbers />
-	<FunActivities />
-	<CapturedMoments />
-{/if}
+<Hero />
+<Insights />
+<Numbers />
+<FunActivities />
+<CapturedMoments />
