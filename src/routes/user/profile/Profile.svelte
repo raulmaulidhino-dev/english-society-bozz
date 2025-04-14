f72db0515a3e217cd4f2994829b68596b61ce7a8
Raul Maulidhino <raulahmadm314@gmail.com>
2025-04-14 23:38:06 +0700

    fix: resolve small issue on bio view

--- src/routes/user/profile/Profile.svelte
+++ src/routes/user/profile/Profile.svelte
@@ -39,7 +39,7 @@
                 </div>
                 <h2 class="text-2xl text-primary font-bold capitalize mt-14 break-words">{user?.full_name || "Full Name"}</h2>
                 <p class="text-sm text-slate-500 break-words">{user?.username || "username" } - <span class="font-bold text-secondary">{ user.roles ? formatUserRoles(user.roles).join(" | ") : "User" }</span></p>
-                <p class="text-sm break-words">{user?.bio || "No bio yet."}</p>
+                <p class="text-sm break-words whitespace-pre-wrap">{user?.bio || "No bio yet."}</p>
             </section>
             <section class="border-t-2 border-slate-200 py-4 px-4 flex flex-col gap-2">
                 <button onclick={goToProfileEdit} class="text-sm text-primary text-center font-semibold w-full sm:w-fit border-2 border-primary py-2 px-8 rounded-full
