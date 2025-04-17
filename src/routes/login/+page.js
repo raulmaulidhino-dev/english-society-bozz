959394569a1461c4da954fda31cf880a7cb5bcd8
Raul Maulidhino <raulahmadm314@gmail.com>
2025-04-17 18:56:21 +0700

    fix: resolve redirect when auth users are already logged in

--- src/routes/login/+page.js
+++ src/routes/login/+page.js
@@ -1,11 +1,11 @@
 export const ssr = false;
 
-import { get } from "svelte/store"
-import { redirect } from "@sveltejs/kit";
-
-import { isLoggedIn } from '$lib/stores/auth';
+import { db } from "$lib/supabase";
 
 export async function load() {
-    const isUserLoggedIn = get(isLoggedIn);
-    if (isUserLoggedIn) throw redirect(302, '/user/profile');
+    const {
+        data: { session }
+    } = await db.auth.getSession();
+
+    if (session?.user) window.location.replace('/user/profile');
 }
