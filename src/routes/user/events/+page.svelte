ee97a2a70b946818219fcdc9b2f96060595d5473
Raul Maulidhino <raulahmadm314@gmail.com>
2025-04-17 17:52:07 +0700

    feat: add +page for the events

---
+++ src/routes/user/events/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import UserEvents from './UserEvents.svelte';
+    let { data } = $props();
+</script>
+
+<UserEvents events={data.events} pageCount={data.pageCount} pageNum={data.page} />
