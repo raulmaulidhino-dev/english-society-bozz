dbba40968196b439457a1b877643a0476108796b
Raul Maulidhino <raulahmadm314@gmail.com>
2025-04-19 16:45:50 +0700

    feat: add meta title and description for Achievements page

--- src/routes/achievements/+page.svelte
+++ src/routes/achievements/+page.svelte
@@ -2,4 +2,9 @@
     import UnderDevelopment from "$lib/components/UnderDevelopment.svelte";
 </script>
 
+<svelte:head>
+	<title>Achievements | ES-Bozz</title>
+	<meta name="description" content="English Society-Bozz Achievements" />
+</svelte:head>
+
 <UnderDevelopment />
