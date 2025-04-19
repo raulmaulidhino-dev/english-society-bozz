f786bfc9d80d49f20f88571ade4a9a335c443324
Raul Maulidhino <raulahmadm314@gmail.com>
2025-04-19 11:02:00 +0700

    style: add blue color for neutral Notification type

--- src/lib/components/Notification.svelte
+++ src/lib/components/Notification.svelte
@@ -20,7 +20,7 @@
 </script>
 
 {#if visible}
-    <div class="fixed top-5 left-5 right-5 sm:left-1/2 sm:right-0 z-[999] sm:-translate-x-1/2 flex items-center justify-between p-4 rounded-lg shadow-xl text-white max-w-xl transition-opacity duration-500 ease-in-out cursor-pointer { type === "error" ? 'bg-red-500' : 'bg-green-500' }">
+    <div class="fixed top-5 left-5 right-5 sm:left-1/2 sm:right-0 z-[999] sm:-translate-x-1/2 flex items-center justify-between p-4 rounded-lg shadow-xl text-white max-w-xl transition-opacity duration-500 ease-in-out cursor-pointer { type === "error" ? 'bg-red-500' : type === "success" ? 'bg-green-500' : 'bg-blue-500' }">
         <p>{ message }</p>
         <button onclick={hideNotification} class="ml-2"><Icon src={CloseBtn} solid size="30" /></button>
     </div>
