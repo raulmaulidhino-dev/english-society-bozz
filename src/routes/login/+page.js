18d1cf11995b4db093568675db2bba80a29d126f
Raul Maulidhino <raulahmadm314@gmail.com>
2025-04-19 16:55:44 +0700

    feat: add meta title and description for Login page

--- src/routes/login/+page.js
+++ src/routes/login/+page.js
@@ -6,4 +6,11 @@ import { isLoggedIn } from "$lib/stores/auth";
 export async function load() {
     const status = get(isLoggedIn);
     if (status) window.location.replace('/user/profile');
+        
+    const meta = {
+        title: "Login | ES-Bozz",
+        description: "Securely log in to you ES-Bozz account to manage your account, settings, data, and resources based on your role(s)"
+    }
+
+    return { meta };
 }
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,11 @@
 <script>
     import Login from './Login.svelte';
+    import { defaultMeta } from '$lib/meta/meta';
 </script>
+
+<svelte:head>
+	<title>{ data.meta?.title ?? defaultMeta.title }</title>
+	<meta name="description" content={ data.meta?.description ?? defaultMeta.description } />
+</svelte:head>
 
 <Login />
