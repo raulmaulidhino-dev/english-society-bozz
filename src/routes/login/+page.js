8b6fcaf1f67772dbda0c18b0b96fc93b9e37eddb
Raul Maulidhino <raulahmadm314@gmail.com>
2025-04-19 18:41:46 +0700

    fix: resolve login status handler before loading the login page

--- src/routes/login/+page.js
+++ src/routes/login/+page.js
@@ -1,11 +1,13 @@
 export const ssr = false;
 
-import { get } from "svelte/store"; 
-import { isLoggedIn } from "$lib/stores/auth";
+import { db } from '$lib/supabase';
 
 export async function load() {
-    const status = get(isLoggedIn);
-    if (status) window.location.replace('/user/profile');
+    const {
+        data: { session }
+    } = await db.auth.getSession();
+    
+    if (session?.user) window.location.replace('/user/profile');
         
     const meta = {
         title: "Login | English Society-Bozz",
