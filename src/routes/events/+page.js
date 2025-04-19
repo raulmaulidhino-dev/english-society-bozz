57456f9c6b541ec71d2033130f35745197cb196a
Raul Maulidhino <raulahmadm314@gmail.com>
2025-04-19 16:47:16 +0700

    feat: add dynamic meta title and description for Events page

--- src/routes/events/+page.js
+++ src/routes/events/+page.js
@@ -1,6 +1,7 @@
 export const ssr = false;
 
 import axios from "axios";
+import { error } from "@sveltejs/kit";
 
 import { BACKEND_URL } from "$lib/config/config";
 
@@ -9,17 +10,24 @@ export async function load({ url }) {
     const limit = 12;
     const offset = (page - 1) * limit;
 
+    const meta = {
+        title: "Events | ES-Bozz",
+        description: "Stay up-to-date with upcoming ES-Bozz events, workshops, and gatherings. Join us in celebrating milestones, learning new things, and building a vibrant community."
+    }
+
     try {
         const res = await axios.get(`${BACKEND_URL}/events?limit=${limit}&offset=${offset}`);
         const events = res.data.data;
         const count = res.data.count;
         const pageCount = Math.ceil(count / limit);
         
-        if (page > pageCount || page < 1) return {};
+        if (page > pageCount || page < 1) throw error(404, "Not Found");
+
+        meta.title = `Events - Page ${page} | ES-Bozz`;
         
-        return { events, pageCount, page }
+        return { events, pageCount, page, meta };
     } catch (error) {
-        return { error };
+        return { error, meta };
     }
 
 }
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,7 +1,14 @@
 <script>
     import Events from './Events.svelte';
+    import { defaultMeta } from '$lib/meta/meta';
+
     let { data } = $props();
 
 </script>
+
+<svelte:head>
+	<title>{ data.meta?.title ?? defaultMeta.title }</title>
+	<meta name="description" content={ data.meta?.description ?? defaultMeta.description } />
+</svelte:head>
 
 <Events events={data.events} pageCount={data.pageCount} pageNum={data.page} error={data.error || null} />
