41d06d28b0cc0f4e42cfbd3dc02319327fc2ccd8
Raul Maulidhino <raulahmadm314@gmail.com>
2025-04-19 16:45:20 +0700

    feat: add meta title and description for About Us page

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -2,4 +2,9 @@
     import UnderDevelopment from "$lib/components/UnderDevelopment.svelte";
 </script>
 
+<svelte:head>
+	<title>About Us | ES-Bozz</title>
+	<meta name="description" content="About English Society-Bozz" />
+</svelte:head>
+
 <UnderDevelopment />
