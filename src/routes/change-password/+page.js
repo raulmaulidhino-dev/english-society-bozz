c7111a8a72c30f027febf6bb06cd4c727bc7f0e4
Raul Maulidhino <raulahmadm314@gmail.com>
2025-04-19 07:59:19 +0700

    feat: add handler before loading the change password page

---
+++ src/routes/change-password/+page.js
@@ -0,0 +1,12 @@
+export const ssr = false;
+
+import { db } from "$lib/supabase";
+import { error } from "@sveltejs/kit";
+
+export async function load() {
+    const {
+        data: { session }
+    } = await db.auth.getSession();
+    
+    if (!session?.user) throw error(404, "Not found");
+}
