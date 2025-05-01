9382fa9c25d345b61a79db2c432eaacaa6f4792e
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-01 10:11:17 +0700

    feat: add sveltePreprocess plugin

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sveltePreprocess } from 'svelte-preprocess';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -8,7 +10,12 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	},
-	preprocess: vitePreprocess()
+	preprocess: [
+		vitePreprocess(),
+		sveltePreprocess({
+			typescript: true,
+		})
+	],
 };
 
 export default config;
