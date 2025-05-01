3122319bf6fa8326b219afe7581d7c42901f5dc6
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-01 10:32:42 +0700

    feat: migrate to typescript

---
+++ src/routes/user/profile/edit/+page.ts
@@ -0,0 +1,67 @@
+export const ssr = false;
+
+import axios from "axios";
+import { db } from "$lib/supabase.js";
+import { BACKEND_URL } from "$lib/config/config";
+
+import { error, isHttpError, isRedirect } from "@sveltejs/kit";
+import type { ErrorResponse } from "$lib/types/error/error.js";
+import type { Meta } from "$lib/types/meta/meta.js";
+import type { UserProfile } from "$lib/types/user/user.js";
+
+const fetchUserProfile = async (token: string) => {
+    let userProfile: UserProfile | null = null;
+
+    if (!token) {
+        throw error(404, "Not found");
+    }
+
+    try {            
+        const response = await axios.get<UserProfile>(`${BACKEND_URL}/user/profile`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        userProfile = response.data;
+    } catch (err: unknown) {
+        if (axios.isAxiosError<ErrorResponse>(err)) {
+            if (err.response?.status === 404) {
+                throw error(404, "Not found");
+            } else {
+                throw error(500, "Unexpected error");
+            }
+            
+        } else if (isRedirect(err) || isHttpError(err)) {
+            throw err;
+        } else {
+            throw error(500, "Unexpected error");
+        }
+    }
+    
+    return userProfile;
+};
+
+export async function load({ url }) {
+    const {
+        data: { session }
+    } = await db.auth.getSession();
+
+    const token: string | undefined = session?.access_token;
+
+    if (!token) {
+        throw error(404, "Not found");
+    }
+
+    const newAvatarUrl: string | null = url.searchParams?.get('newAvatarUrl');
+
+    let userProfile = await fetchUserProfile(token);
+    if (userProfile && newAvatarUrl) userProfile = {...userProfile, new_avatar_url: newAvatarUrl};
+
+    const meta: Meta = {
+        title: `Edit Profile`,
+        description: `Edit my profile data.`
+    }
+
+    return { userProfile, meta };
+}
