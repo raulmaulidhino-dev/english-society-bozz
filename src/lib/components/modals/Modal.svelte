ac747586777e61894b91e28e02318793e407e797
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-02 23:05:19 +0700

    feat: add basic modal component

---
+++ src/lib/components/modals/Modal.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  const { children } = $props();
+</script>
+
+{#if children}
+  <div class="fixed inset-0 bg-black/50 flex items-center justify-center z-[999]">
+    <div class="bg-white p-6 rounded-lg shadow-lg max-w-md w-full mx-4">
+      {@render  children()}
+    </div>
+  </div>
+{/if}
