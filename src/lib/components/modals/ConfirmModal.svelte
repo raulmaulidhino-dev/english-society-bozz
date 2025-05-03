88bbeebd7edd22e99cef6dbca47d7673bb5ebc7b
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-03 17:45:37 +0700

    fix: use show props instead of conditional state to prevent null element issues

--- src/lib/components/modals/ConfirmModal.svelte
+++ src/lib/components/modals/ConfirmModal.svelte
@@ -19,20 +19,18 @@
 
 </script>
 
-{#if show}
-	<Modal>
-		{#snippet children()}
-            <div class="mb-4 flex items-center gap-4">
-                <div class="text-red-500 bg-red-200 rounded-full p-2">
-                    <Icon src={WarningIcon} size="28"/>
-                </div>
-                <h2 class="text-xl font-semibold">{title}</h2>
-            </div>
-            <p class="text-sm mb-[1.5em]">{message}</p>
-            <div class="flex justify-stretch items-stretch gap-2">
-                <button onclick={onCancel} class="grow text-white text-xs font-bold bg-primary border-2 border-primary hover:text-primary hover:bg-secondary p-2 rounded-full">{cancelLabel}</button>
-                <button onclick={onConfirm} class="grow text-white text-xs font-bold bg-red-700 border-2 border-red-700 hover:bg-red-900 hover:border-red-900 p-2 rounded-full">{confirmLabel}</button>
-            </div>
-		{/snippet}
-	</Modal>
-{/if}
+<Modal show={show}>
+  {#snippet children()}
+          <div class="mb-4 flex items-center gap-4">
+              <div class="text-red-500 bg-red-200 rounded-full p-2">
+                  <Icon src={WarningIcon} size="28"/>
+              </div>
+              <h2 class="text-xl font-semibold">{title}</h2>
+          </div>
+          <p class="text-sm mb-[1.5em]">{message}</p>
+          <div class="flex justify-stretch items-stretch gap-2">
+              <button onclick={onCancel} class="grow text-white text-xs font-bold bg-primary border-2 border-primary hover:text-primary hover:bg-secondary p-2 rounded-full">{cancelLabel}</button>
+              <button onclick={onConfirm} class="grow text-white text-xs font-bold bg-red-700 border-2 border-red-700 hover:bg-red-900 hover:border-red-900 p-2 rounded-full">{confirmLabel}</button>
+          </div>
+  {/snippet}
+</Modal>
