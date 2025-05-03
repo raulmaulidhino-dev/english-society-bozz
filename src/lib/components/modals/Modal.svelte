f0872b154305cf1d72d9f140ac18e96114a2373e
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-03 17:44:35 +0700

    fix: change visibility show props from exist-or-not to hidden-or-not to prevent null element issues

--- src/lib/components/modals/Modal.svelte
+++ src/lib/components/modals/Modal.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  const { children } = $props();
+  const { children, show = false } = $props();
 </script>
 
 {#if children}
-  <div class="fixed inset-0 bg-black/50 flex items-center justify-center z-[999]">
+  <div class={`fixed inset-0 bg-black/50 flex items-center justify-center z-[999] ${ show ? "" : "hidden" }`}>
     <div class="bg-white p-6 rounded-lg shadow-lg max-w-md w-full mx-4">
       {@render  children()}
     </div>
