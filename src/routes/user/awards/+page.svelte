77e10834f7d3389a444acea236114cb20d896e93
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-05 17:29:54 +0700

    featL add user awards main page

---
+++ src/routes/user/awards/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import UserAwards from './UserAwards.svelte';
+    import { defaultMeta } from '$lib/meta/meta';
+    import type { AwardResponse } from '$lib/types/award/award';
+    import type { Meta } from '$lib/types/meta/meta';
+
+    interface Props {
+        data: {
+            meta: Meta;
+            awards: AwardResponse[];
+            pageCount: number;
+            page: number;
+        }
+    }
+
+    let { data }: Props = $props();
+    
+</script>
+
+<svelte:head>
+	<title>{ data.meta?.title ?? defaultMeta.title }</title>
+	<meta name="description" content={ data.meta?.description ?? defaultMeta.description } />
+</svelte:head>
+
+<UserAwards awards={data.awards} pageCount={data.pageCount} pageNum={data.page} />
