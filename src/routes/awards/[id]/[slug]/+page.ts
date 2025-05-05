486062553e7f81adc77af046134fdfbc52959a98
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-05 13:30:21 +0700

    feat: add handler before loading the dynamic award single page

---
+++ src/routes/awards/[id]/[slug]/+page.ts
@@ -0,0 +1,44 @@
+export const ssr = false;
+
+import axios from "axios";
+import { isHttpError, error, isRedirect, redirect } from "@sveltejs/kit";
+
+import { BACKEND_URL } from "$lib/config/config";
+
+import type { AwardResponse } from "$lib/types/award/award.js";
+
+export async function load({ params }) {
+    const { id, slug } = params;
+
+    let award: AwardResponse | null = null;
+
+    try {
+        const res = await axios.get<AwardResponse>(`${BACKEND_URL}/awards/${id}/${slug}`);
+        award = res.data;
+
+        if (slug !== award.slug) throw redirect(301, `/awards/${id}/${award.slug}`);
+
+        const meta = {
+            title: `${award.name} - Award`,
+            description: `${award.award_categories.category} • ${award.award_levels.level} • Awarded by ${award.student_name}`,
+        }
+
+        return { award, meta };
+
+    } catch (err: unknown) {
+        if (axios.isAxiosError(err)) {
+            if (err.response?.status === 301) {
+                if (award) throw redirect(301, `/events/${id}/${award.slug}`);
+                else throw error(404, "Not found");
+            } else if (err.response?.status === 404) {
+                throw error(404, "Not found");
+            } else {
+                throw error(500, "Unexpected error");
+            }
+        } else if (isRedirect(err) || isHttpError(err)) {
+            throw err;
+        } else {
+            throw error(500, "Unexpected error");
+        }
+    }
+}
