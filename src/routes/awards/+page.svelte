96cfc46da7cae02d8f7f9e72cd1443d5a323ebf0
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-05 13:29:38 +0700

    feat: add main awards page

---
+++ src/routes/awards/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { AxiosError } from 'axios';
+	
+	import { defaultMeta } from '$lib/meta/meta';
+
+	import Awards from './Awards.svelte';
+
+	import type { Meta } from '$lib/types/meta/meta';
+	import type { AwardResponse } from '$lib/types/award/award';
+
+	interface Props {
+			data: {
+					meta: Meta;
+					awards?: AwardResponse[];
+					pageCount?: number;
+					page?: number;
+					err?: AxiosError;
+			}
+	}
+
+	let { data }: Props = $props();
+
+</script>
+
+<svelte:head>
+	<title>{ data.meta?.title ?? defaultMeta.title }</title>
+	<meta name="description" content={ data.meta?.description ?? defaultMeta.description } />
+</svelte:head>
+
+<Awards awards={data.awards} pageCount={data.pageCount} pageNum={data.page} error={data.err} />
