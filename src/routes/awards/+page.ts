bf09d40d36cccbef278dc0b118eed03b910c0835
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-05 13:27:42 +0700

    feat: add handler before loading the main awards page

---
+++ src/routes/awards/+page.ts
@@ -0,0 +1,47 @@
+export const ssr = false;
+
+import axios from 'axios';
+import { error, isHttpError, isRedirect } from '@sveltejs/kit';
+
+import { BACKEND_URL } from '$lib/config/config';
+
+import type { AwardResponse, AwardsResponse } from '$lib/types/award/award.js';
+import type { Meta } from '$lib/types/meta/meta.js';
+import type { ErrorResponse } from '$lib/types/error/error.js';
+
+export async function load({ url }) {
+  const page: number = parseInt(url.searchParams.get('page') ?? '1');
+  const limit: number = 8;
+  const offset: number = (page - 1) * limit;
+
+  const meta: Meta = {
+    title: 'Awards | English Society-Bozz',
+    description:
+      "From awards to major milestones, see how far we've come. This section showcases our proudest moments and the progress we've made over time.",
+  };
+
+  try {
+    const res = await axios.get<AwardsResponse>(
+      `${BACKEND_URL}/awards?limit=${limit}&offset=${offset}`
+    );
+    const awards: AwardResponse[] = res.data.data;
+    const count: number = res.data.count;
+    const pageCount: number = Math.ceil(count / limit);
+
+    if ((page > pageCount && count > 0) || page < 1)
+      throw error(404, 'Not Found');
+
+    meta.title = `Awards - Page ${page} | English Society-Bozz`;
+
+    return { awards, pageCount, page, meta };
+  } catch (err: unknown) {
+    if (axios.isAxiosError<ErrorResponse>(err)) {
+      if (err?.response?.status === 404) throw error(404, 'Not Found');
+      return { err, meta };
+    } else if (isRedirect(err) || isHttpError(err)) {
+      throw err;
+    } else {
+      throw error(500, 'Unexpected error');
+    }
+  }
+}
