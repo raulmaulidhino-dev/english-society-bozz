adbb52f5f6dae3411345b8d1d8454c18177ccdd2
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-06 02:06:15 +0700

    feat: add Award Editor or Announcer's  main page

---
+++ src/routes/user/awards/announce/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import AwardEditor from "./AwardEditor.svelte";
+    import { defaultMeta } from "$lib/meta/meta";
+    import type { AwardCategory, AwardLevel, AwardTitle, StudentClass } from "$lib/types/award/award";
+    import type { UserProfile } from "$lib/types/user/user";
+    import type { Meta } from "$lib/types/meta/meta";
+
+    interface Props {
+        data: {
+            userProfile: UserProfile;
+            awardCategories: AwardCategory[];
+            awardLevels: AwardLevel[];
+            awardTitles: AwardTitle[];
+            studentClasses: StudentClass[];
+            meta: Meta;
+        }
+    }
+
+    let { data }: Props = $props(); 
+
+</script>
+
+<svelte:head>
+	<title>{ data.meta?.title ?? defaultMeta.title }</title>
+	<meta name="description" content={ data.meta?.description ?? defaultMeta.description } />
+</svelte:head>
+
+<AwardEditor 
+    userProfile={data.userProfile}
+    awardCategories={data.awardCategories}
+    awardLevels={data.awardLevels}
+    awardTitles={data.awardTitles}
+    studentClasses={data.studentClasses}
+/>
