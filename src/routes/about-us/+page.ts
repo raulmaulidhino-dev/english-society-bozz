ab6545ed0f89e505a0ca0c5d59e61973ba2494f9
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-07 00:55:41 +0700

    feat: add handler before loading the main About Us page

---
+++ src/routes/about-us/+page.ts
@@ -0,0 +1,13 @@
+export const ssr = false;
+
+import { db } from '$lib/supabase';
+import { error } from '@sveltejs/kit';
+
+export async function load() {
+    const { data: esbozzData, error: esbozzDataFetchingError } = await db.from("esbozz_data").select("mission, focus, join_us_url, activities");    
+    const { data: esbozzOfficers, error: officersFetchingError } = await db.from("esbozz_officers").select("president, vice_president, secretary_1, secretary_2, treasurer_1, treasurer_2, it_manager_1, it_manager_2, officers"); 
+
+    if (esbozzDataFetchingError || officersFetchingError) throw error(500, "Unexpected error");
+    
+    return { esbozzData: esbozzData[0], esbozzOfficers: esbozzOfficers[0] };
+}
