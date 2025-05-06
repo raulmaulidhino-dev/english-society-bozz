11cb72288db5bb2a98e7182cfb685fde7b465355
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-07 00:54:50 +0700

    feat: add types for dynamic About Us page data

---
+++ src/lib/types/esbozz/data/esbozz.d.ts
@@ -0,0 +1,6 @@
+export interface ESBozzData {
+  focus: string;
+  mission: string;
+  activities: string[];
+  join_us_url: string | null;
+}
---
+++ src/lib/types/esbozz/officer/officers.d.ts
@@ -0,0 +1,11 @@
+export interface ESBozzOfficers {
+  president: string | null
+  vice_president: string | null
+  secretary_1: string | null
+  secretary_2: string | null
+  treasurer_1: string | null
+  treasurer_2: string | null
+  it_manager_1: string | null
+  it_manager_2: string | null
+  officers: string[] | null
+}
