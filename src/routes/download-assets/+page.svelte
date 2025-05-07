6ea4204ca9fe0b28a67f42739ec1606a83f638c9
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-08 01:21:53 +0700

    feat: add main download assets page

---
+++ src/routes/download-assets/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Download from "./Download.svelte";
+</script>
+
+<svelte:head>
+	<title>Download Assets | English Society-Bozz</title>
+	<meta name="description" content="Download English Society-Bozz logos and components in various formats." />
+</svelte:head>
+
+<Download />
