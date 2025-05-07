b56b7d08ddc32954c2c6b500f8ee070de3b7e536
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-07 08:08:30 +0700

    feat: add About Us Edit main page

---
+++ src/routes/about-us/edit/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import AboutUsEdit from "./AboutUsEdit.svelte";
+	import type { ESBozzData } from "$lib/types/esbozz/data/esbozz";
+	import type { ESBozzOfficers } from "$lib/types/esbozz/officer/officers";
+
+	interface Props {
+		data: {
+			esbozzData: ESBozzData;
+			esbozzOfficers: ESBozzOfficers;
+		}
+	}
+
+	let { data }: Props = $props();
+	
+</script>
+
+<svelte:head>
+	<title>About Us (Edit) | English Society-Bozz</title>
+	<meta name="description" content="Edit the content of English Society-Bozz About Page" />
+</svelte:head>
+
+<AboutUsEdit esbozzData={data.esbozzData} esbozzOfficers={data.esbozzOfficers} />
