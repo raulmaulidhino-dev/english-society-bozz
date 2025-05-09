084ee41a3985f74891bdb1904991761992fa010a
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-09 14:35:41 +0700

    feat: add Avatar View Modal svelte component to see a user's avatar in larger view

---
+++ src/lib/components/modals/AvatarViewModal.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let { show = $bindable(false), avatar } = $props();
+
+  let handleClick = (e: MouseEvent) => {
+    if (e.target === e.currentTarget) show = false;
+  }
+
+</script>
+
+<button onclick={handleClick} class={`fixed inset-0 bg-black/70 flex items-center justify-center z-[999] ${ show ? "" : "hidden" }`}>
+  <div class="rounded-full shadow-xl max-w-md w-full mx-10">
+    <img src={avatar} alt="My Avatar" class="w-full rounded-[inherit]" />
+  </div>
+</button>
