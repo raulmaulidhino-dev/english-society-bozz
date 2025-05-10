f2a59867564a969da5b703e6ea56abae9a076462
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-10 10:06:25 +0700

    style: enhance the appearance of 404 HTTP errror awards not found

--- src/routes/awards/Awards.svelte
+++ src/routes/awards/Awards.svelte
@@ -78,12 +78,12 @@
         <p>It looks like there aren't any awards here yet...</p>   
       {/if}
     {:else}
-        <section data-aos="zoom-in" class="bg-white pt-16 pb-8 px-4 max-w-xl rounded-2xl mx-auto shadow-lg col-span-2">
+        <section data-aos="zoom-in" class="bg-white pt-16 pb-8 px-4 max-w-xl rounded-2xl mx-auto shadow-lg col-span-full">
             <div class="mx-auto px-4 max-w-screen-sm text-center flex flex-col justify-center items-center">
                 <h1 class="text-8xl tracking-tight font-extrabold text-primary lg:text-9xl animate-bounce">4<span class="text-secondary">0</span>4</h1>
                 <div class="bg-slate-200 h-[2px] w-full rounded-full mb-4"></div>
                 <p class="mb-4 text-2xl tracking-tight font-bold text-gray-900 md:text-3xl">Oops! No awards found here.</p>
-                <p class="mb-6 text-md font-light text-gray-500 max-w-80">Looks like you have reached <span class="font-bold">"The End"</span> of the page!</p>
+                <p class="mb-6 text-md font-light text-gray-500 max-w-80">Looks like you have reached <span class="font-bold">"The End"</span> of the page or got lost!</p>
                 <a href="/awards" class="text-sm text-white text-center bg-primary font-bold w-full sm:w-fit border-2 border-primary py-2 px-2 md:px-8 rounded-full hover:text-primary hover:bg-secondary inline-block">SEE OUR RECENT AWARDS</a>
             </div>
         </section>        
