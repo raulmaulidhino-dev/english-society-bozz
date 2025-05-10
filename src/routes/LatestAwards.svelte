bfa0643d3d7843b08e28047830a8e4f15add1a1e
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-10 10:05:11 +0700

    style: enhance the appearance of 404 HTTP error awards not found

--- src/routes/LatestAwards.svelte
+++ src/routes/LatestAwards.svelte
@@ -33,21 +33,23 @@
                 <p>It looks like there aren't any awards here yet...</p>   
             {/if}
         {:else}
-            <section data-aos="zoom-in" class="bg-white pt-16 pb-8 px-4 max-w-xl rounded-2xl mx-auto shadow-lg col-span-2">
+            <section data-aos="zoom-in" class="bg-white pt-16 pb-8 px-4 max-w-xl rounded-2xl mx-auto shadow-lg col-span-full justify-self-center place-self-center center">
                 <div class="mx-auto px-4 max-w-screen-sm text-center flex flex-col justify-center items-center">
                     <h1 class="text-8xl tracking-tight font-extrabold text-primary lg:text-9xl animate-bounce">4<span class="text-secondary">0</span>4</h1>
                     <div class="bg-slate-200 h-[2px] w-full rounded-full mb-4"></div>
                     <p class="mb-4 text-2xl tracking-tight font-bold text-gray-900 md:text-3xl">Oops! No awards found here.</p>
                     <p class="mb-6 text-md font-light text-gray-500 max-w-80">Our awards failed to load. A refresh may resolve the issue 🤠</p>
-                    <a href="/awards" class="text-sm text-white text-center bg-primary font-bold w-full sm:w-fit border-2 border-primary py-2 px-2 md:px-8 rounded-full hover:text-primary hover:bg-secondary inline-block">SEE OUR RECENT AWARDS</a>
+                    <button onclick={() => location.reload()} class="text-sm text-white text-center bg-primary font-bold w-full sm:w-fit border-2 border-primary py-2 px-2 md:px-8 rounded-full hover:text-primary hover:bg-secondary inline-block">REFRESH PAGE</button>
                 </div>
             </section>        
         {/if}
     </section>
-    <section class="flex justify-center items-center">
-        <a data-aos="zoom-in" href="/awards" class="text-secondary text-sm px-4 py-3 font-bold border-2 border-primary bg-primary hover:bg-secondary hover:text-primary rounded-full mt-[4rem] mx-auto flex items-center justify-center gap-2">
-            <span>EXPLORE MORE!</span>
-            <ArrowRightIcon size="16" class="ml-auto" />
-        </a>
-    </section>
+    {#if awards}
+        <section class="flex justify-center items-center">
+            <a data-aos="zoom-in" href="/awards" class="text-secondary text-sm px-4 py-3 font-bold border-2 border-primary bg-primary hover:bg-secondary hover:text-primary rounded-full mt-[4rem] mx-auto flex items-center justify-center gap-2">
+                <span>EXPLORE MORE!</span>
+                <ArrowRightIcon size="16" class="ml-auto" />
+            </a>
+        </section>
+    {/if}
 </section>
