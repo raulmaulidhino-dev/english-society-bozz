9f526d1fcb3302df22db5492109536ce9ceeec62
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-11 10:14:44 +0700

    feat: add og and twitter meta configuration in link sharing

--- src/routes/download-assets/+page.svelte
+++ src/routes/download-assets/+page.svelte
@@ -5,6 +5,21 @@
 <svelte:head>
 	<title>Download Assets | English Society-Bozz</title>
 	<meta name="description" content="Download English Society-Bozz logos and components in various formats." />
+
+	<!-- Facebook Meta Tags -->
+	<meta property="og:title" content="Download Assets | English Society-Bozz" />
+	<meta property="og:description" content="Download English Society-Bozz logos and components in various formats." />
+	<meta property="og:image" content="https://english-society-bozz.vercel.app/assets/logos/esbozz-logo-og.jpg" />
+	<meta property="og:url" content="https://english-society-bozz.vercel.app/download-assets" />
+	<meta property="og:type" content="website" />
+
+	<!-- Twitter Meta Tags -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:domain" content="english-society-bozz.vercel.app" />
+	<meta property="twitter:url" content="https://english-society-bozz.vercel.app/download-assets" />
+	<meta property="twitter:title" content="Download Assets | English Society-Bozz" />
+	<meta property="twitter:description" content="Download English Society-Bozz logos and components in various formats." />
+	<meta property="twitter:image" content="https://english-society-bozz.vercel.app/assets/logos/esbozz-logo-og.jpg" />
 </svelte:head>
 
 <Download />
