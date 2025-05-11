bd9474df7bac7d78b10108f9d4fc5b6fe37991ca
Raul Maulidhino <raulahmadm314@gmail.com>
2025-05-11 10:17:23 +0700

    feat: add ogImage and url fallbacks in defaultMeta variable

--- src/lib/meta/meta.ts
+++ src/lib/meta/meta.ts
@@ -1,6 +1,8 @@
 import type { Meta } from "$lib/types/meta/meta";
 
 export const defaultMeta: Meta = {
-    title: "ES-Boz",
-    description: "Welcome to English Society Bozz!"
+    title: "ES-Bozz",
+    description: "Welcome to English Society Bozz!",
+    url: "https://english-society-bozz.vercel.app",
+    ogImage: "https://english-society-bozz.vercel.app/assets/logos/esbozz-logo-og.jpg",
 };
